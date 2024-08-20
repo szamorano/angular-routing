8fbbf7b5360a84341788cb824c4d35b1aa46fd88
Steven Zamorano <stevenzamorano.code@gmail.com>
2024-08-20 15:49:04 -0400

    Rendering routes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import { TasksComponent } from './app/tasks/tasks.component';
-import { routes } from './app/app.routes';
 import { appConfig } from './app/app.config';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
